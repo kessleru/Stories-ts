d603fbfec2f1819b3ce362c5c46aec40b59797c8
kessleru <otaviokesslerustra@hotmail.com>
2026-06-16 19:23:51 -0300

    Refactor code structure for improved readability and maintainability

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,9 @@
-// import './style.css'
+import Slide from './Slide';
 
+const container = document.getElementById('slide');
+const elements = document.getElementById('slide-elements');
+const controls = document.getElementById('slide-controls');
+
+if (container && elements && controls && elements.children.length > 0) {
+  new Slide(container, Array.from(elements.children), controls, 3000);
+}
