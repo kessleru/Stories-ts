b029b115bc837e95374106dc61335b15706394c1
kessleru <otaviokesslerustra@hotmail.com>
2026-06-18 20:53:49 -0300

    Refactor Slide class and update styles for improved functionality and layout

--- src/Slide.ts
+++ src/Slide.ts
@@ -1,17 +1,57 @@
 export default class Slide {
   container: Element;
-  elements: Element[];
+  slides: Element[];
   controls: Element;
   time: number;
+  index: number = 0;
+  slide: Element;
   constructor(
     container: Element,
-    elements: Element[],
+    slides: Element[],
     controls: Element,
     time: number = 5000
   ) {
     this.container = container;
-    this.elements = elements;
+    this.slides = slides;
     this.controls = controls;
     this.time = time;
+
+    this.index = 0;
+    this.slide = this.slides[this.index];
+
+    this.init();
+  }
+  hide(slide: Element) {
+    slide.classList.remove('active');
+  }
+  show(index: number) {
+    this.index = index;
+    this.slide = this.slides[this.index];
+    this.slides.forEach((slide) => this.hide(slide));
+    this.slide.classList.add('active');
+  }
+  prev() {
+    this.show((this.index - 1 + this.slides.length) % this.slides.length);
+    // this.index - 1 >= 0 ? this.index-- : (this.index = this.slides.length - 1);
+  }
+  next() {
+    this.show((this.index + 1) % this.slides.length);
+    // this.index + 1 < this.slides.length ? this.index++ : (this.index = 0);
+  }
+  private addControls() {
+    const prevButton = document.createElement('button');
+    const nextButton = document.createElement('button');
+    this.controls.appendChild(prevButton);
+    this.controls.appendChild(nextButton);
+
+    prevButton.innerText = 'Prev';
+    nextButton.innerText = 'Next';
+
+    nextButton.addEventListener('pointerup', () => this.next());
+    prevButton.addEventListener('pointerup', () => this.prev());
+  }
+  private init() {
+    this.addControls();
+    this.show(this.index);
   }
 }
